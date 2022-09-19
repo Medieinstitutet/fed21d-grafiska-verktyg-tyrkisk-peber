fca8097ca55c0d18b4178c779205358ea9aed7c3
jsilf <jesilfver@gmail.com>
2022-09-19 15:29:48 +0200

    created cookie-banner for mobile and tablet

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+//animation candy on cookie-banner
+gsap.to(".cookie-svg", {
+  duration: 1,
+  fill: "#000",
+  transformOrigin: "60% 60%",
+  rotation: 360,
+  repeat: 0,
+});
